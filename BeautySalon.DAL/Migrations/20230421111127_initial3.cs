1a44813315c27375744a6ebf0692faf83a85a7d3
StormGun <StormGun@DESKTOP-8J69FMB>
2023-04-21 16:27:40 +0500

    bb

---
+++ BeautySalon.DAL/Migrations/20230421111127_initial3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeautySalon.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Clients",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Clients");
+        }
+    }
+}
---
+++ BeautySalon/ViewModels/ServicesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BeautySalon.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySalon.ViewModels
+{
+    internal class ServicesViewModel : ViewModel
+    {
+        private string title = "Услуги";
+        public string Title { get => title; set => Set(ref title,value); }
+
+    }
+}

30f0de04270b597011c5defc9fa0feb9b747362b
StormGun <StormGun@DESKTOP-8J69FMB>
2023-04-26 14:42:34 +0500

    n

---
+++ BeautySalon/Resources/Converters/DateToWeek.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace BeautySalon.Resources.Converters
+{
+    public class DateToWeek : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime week)
+            {
+                return (int)week.Date.DayOfWeek;
+            }
+            else return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+             return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
--- BeautySalon/ViewModels/ServicesViewModel.cs
+++ BeautySalon/ViewModels/ServicesViewModel.cs
@@ -1,4 +1,5 @@
-﻿using BeautySalon.DAL.Entities;
+﻿using BeautySalon.Commands;
+using BeautySalon.DAL.Entities;
 using BeautySalon.Services.Interfaces;
 using BeautySalon.ViewModels.Base;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,16 +18,44 @@ namespace BeautySalon.ViewModels
         private ObservableCollection<Service>? _services;
         public ObservableCollection<Service>? Services { get => _services; set => Set(ref _services, value); }
 
-        private Service? selectedService;
-        public Service? SelectedService { get => selectedService; set => Set(ref selectedService, value); }
+        private Service? selectedServiceL;
+        public Service? SelectedServiceL { get => selectedServiceL; set => Set(ref selectedServiceL, value); }
+
+        private Service? selectedServiceR;
+        public Service? SelectedServiceR { get => selectedServiceR; set => Set(ref selectedServiceR, value); }
 
         private ObservableCollection<ServiceType>? _serviceTypes;
         public ObservableCollection<ServiceType>? ServiceTypes { get => _serviceTypes; set => Set(ref _serviceTypes, value); }
+
+        private ObservableCollection<Service>? selectedServices;
+        public ObservableCollection<Service>? SelectedServices { get => selectedServices; set => Set(ref selectedServices, value); }
+
+
+        #region AddServiceCmd - Command
+        private RelayCommand? addServiceCmd;
+        public RelayCommand? AddServiceCmd => addServiceCmd ??= new(obj => AddService(), obj => CanAddService());
+        private bool CanAddService() => SelectedServiceL != null;
+        private void AddService()
+        {
+            SelectedServices!.Add(SelectedServiceL!);
+        }
+        #endregion
+
+        #region DeleteServiceCmd - Command
+        private RelayCommand? deleteServiceCmd;
+        public RelayCommand? DeleteServiceCmd => deleteServiceCmd ??= new(obj => DeleteService(), obj => CanDeleteService());
+        private bool CanDeleteService() => SelectedServiceR != null;
+        private void DeleteService()
+        {
+            SelectedServices!.Remove(SelectedServiceR!);
+        } 
+        #endregion
 
         public ServicesViewModel()
         {
             Services = servicesService.Services;
             ServiceTypes = servicesService.ServiceTypes;
+            SelectedServices = new();
         }
 
     }

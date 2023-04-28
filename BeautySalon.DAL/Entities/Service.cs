8b5e065a16e427adcb83a4a0d0acff021e6e8339
StormGun <StormGun@DESKTOP-8J69FMB>
2023-04-28 19:02:31 +0500

    all

--- BeautySalon.DAL/Entities/Service.cs
+++ BeautySalon.DAL/Entities/Service.cs
@@ -7,13 +7,16 @@ namespace BeautySalon.DAL.Entities
         private int iD;
         private string? name;
         private decimal? price;
+        private TimeSpan? time;
         private ICollection<Order>? orders;
 
         public int ID { get => iD; set => Set(ref iD, value); }
         public string? Name { get => name; set => Set(ref name, value); }
         public decimal? Price { get => price; set => Set(ref price, value); }
+        public TimeSpan? Time { get => time; set => Set(ref time,value); }
 
         public virtual ICollection<Order>? Orders { get => orders; set => Set(ref orders, value); }
+
         public ServiceType? ServiceType { get; set; }
     }
 }
--- BeautySalon/ViewModels/OrdersViewModels.cs
+++ BeautySalon/ViewModels/OrdersViewModels.cs
@@ -5,7 +5,10 @@ using BeautySalon.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Data;
 
 namespace BeautySalon.ViewModels
 {
@@ -18,13 +21,50 @@ namespace BeautySalon.ViewModels
         private string _title = "Записи";
         public string Title { get => _title; set => Set(ref _title,value); }
 
+        private DateTime selectedDate;
+        public DateTime SelectedDate { get => selectedDate; set { Set(ref selectedDate, value); orderViewSource?.View.Refresh(); } }
+
+        #region Orders
         private ObservableCollection<Order>? orders;
-        public ObservableCollection<Order>? Orders { get => orders; set => Set(ref orders, value); }
+        private CollectionViewSource? orderViewSource;
+        public ICollectionView? OrdersView => orderViewSource?.View;
+        public ObservableCollection<Order>? Orders
+        {
+            get => orders;
+            set
+            {
+                if (Set(ref orders, value))
+                {
+                    orderViewSource = new()
+                    {
+                        Source = value,
+                        SortDescriptions = { new SortDescription(nameof(Order.DateStart), ListSortDirection.Ascending) }
+                    };
+
+                    orderViewSource.Filter += SelectedDateFilter;
+
+                    orderViewSource?.View.Refresh();
+                    OnPropertyChanged(nameof(OrdersView));
+                }
+            }
+        }
+
+        private void SelectedDateFilter(object sender, FilterEventArgs e)
+        {
+            if (e.Item is Order order)
+            {
+                if (order?.DateStart.Value.Date == SelectedDate.Date)
+                    e.Accepted = true;
+                else e.Accepted = false;
+            }
+        }
+        #endregion
 
         public OrdersViewModels(IOrderService orderService)
         {
             this.orderService = orderService;
-            
+            SelectedDate = DateTime.Now;
+
             Orders = this.orderService.Orders;
 
         }

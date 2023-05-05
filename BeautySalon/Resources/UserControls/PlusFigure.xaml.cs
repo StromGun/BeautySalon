7991169c7fecae646e4e42045deaa37f8f6e04ed
StormGun <StormGun@DESKTOP-8J69FMB>
2023-05-05 18:17:22 +0500

    md

---
+++ BeautySalon/Resources/UserControls/PlusFigure.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BeautySalon.Resources.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для PlusFigure.xaml
+    /// </summary>
+    public partial class PlusFigure : UserControl
+    {
+        public PlusFigure()
+        {
+            InitializeComponent();
+        }
+
+        public Brush FillColor
+        {
+            get { return (Brush)GetValue(FillColorProperty); }
+            set { SetValue(FillColorProperty, value); }
+        }
+        public static readonly DependencyProperty FillColorProperty =
+            DependencyProperty.Register("FillColor", typeof(Brush), typeof(WatermarkTextBox), new PropertyMetadata(SystemColors.InactiveSelectionHighlightBrush));
+    }
+}
